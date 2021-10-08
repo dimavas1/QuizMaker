6b188fc816aa00be454ea755c9820a9abea5712b
dimavas1 <dima.vasserfirer@gmail.com>
2021-10-07 21:26:07 -0500

    update ConsoleKeyInfoToQuestionIndex to return null ,  remove questionIndex parameter. addind pad numbers as user inputs and formating.

--- QuizMaker/UI.cs
+++ QuizMaker/UI.cs
@@ -12,14 +12,14 @@ namespace QuizMaker
         /// <returns>Key selected by user, -1 if invaild</returns>
         public static int? PrintQuestionAndGetAnswer(QnA question)
         {
-            
+
             Console.Clear();
             Console.WriteLine(question.Question);
             Console.WriteLine("Select your unswer:");
 
             for (int i = 0; i < question.Answers.Count; i++)
             {
-                Console.WriteLine($"{i+1}.  {question.Answers[i]}");
+                Console.WriteLine($"{i + 1}.  {question.Answers[i]}");
             }
 
             return ConsoleKeyInfoToQuestionIndex(Console.ReadKey(), question.Answers.Count);
@@ -31,7 +31,7 @@ namespace QuizMaker
         /// <param name="key">User key inpur</param>
         /// <param name="total">total answers</param>
         /// <returns>
-        /// -1 if wrong key
+        /// null wrong key
         /// 0 for first answer 
         /// 1 for second answer 
         /// 2 for third answer 
@@ -39,41 +39,37 @@ namespace QuizMaker
         /// </returns>
         public static int? ConsoleKeyInfoToQuestionIndex(ConsoleKeyInfo key, int total)
         {
-            int questionIndex = -1;
 
             switch (key.Key)
             {
+                case ConsoleKey.NumPad1:
                 case ConsoleKey.D1:
-                    questionIndex = 0;
-                    break;
-
-                case ConsoleKey.D2:
-                    questionIndex = 1;
-                    break;
+                    return 0;
                 
+                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
+                    return 1;
+
+                case ConsoleKey.NumPad3:
                 case ConsoleKey.D3:
                     if (total == 3 || total == 4)
                     {
-                        questionIndex = 2;
-                    }                    
-                    break;
-                
-                case ConsoleKey.D4:
-                    if (total == 4)
-                    {
-                        questionIndex = 3;
+                        return 2;
                     }
                     break;
 
+                case ConsoleKey.NumPad4:
+                case ConsoleKey.D4:
+                    if (total == 4)
+                    {
+                        return 3;
+                    }
+                    break;
+
+
             }
 
-            if (questionIndex == -1)
-            {
-                return null;
-            }
-
-            return questionIndex;
-            ;
+            return null;
         }
 
         /// <summary>
@@ -93,7 +89,7 @@ namespace QuizMaker
         /// True if user selected Y to continue play
         /// False if user selected any other key
         /// </returns>
-        public static bool AskUserToContinue(int correct,int wrong)
+        public static bool AskUserToContinue(int correct, int wrong)
         {
             bool play = false;
             Console.Clear();
@@ -109,6 +105,6 @@ namespace QuizMaker
 
             return play;
         }
-        
+
     }
 }
